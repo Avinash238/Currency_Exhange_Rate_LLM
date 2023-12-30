f126c93afd8cd313a47614b66f092684b9ecc4af
Avinash Sharma <96874114+Avinash238@users.noreply.github.com>
2023-12-31 02:16:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from currency_correlation_analysis import analyze_correlation
 
 st.title("Currency Correlation Analysis App")
 
-# Gather user inputs (e.g., through form components)
 currencies = st.text_area("Enter Currencies (comma-separated):", "USD, EUR, JPY, GBP")
 
-# Button to trigger analysis
+
 if st.button("Analyze Currency Correlation"):
     st.write("### Analyzing...")
 
